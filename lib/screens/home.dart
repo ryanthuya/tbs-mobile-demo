7b718426c87ce26a6ba4cb93a1f2b76d2bd56c86
Min Thu Ya Kyaw <minthuyakyaw@gmail.com>
2022-09-02 05:34:01 +0630

    Added  GetWidget Package

---
+++ lib/screens/home.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+import '../env.sample.dart';
+import '../models/student.dart';
+import 'create.dart';
+import 'details.dart';
+
+class Home extends StatefulWidget {
+  // const Homepage({Key? key}) : super(key: key);
+  const Home({Key? key}) : super(key: key);
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  late Future<List<Student>> students;
+  final studentListKey = GlobalKey<HomeState>();
+
+  // final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    students = getStudentList();
+    //Firebase Token
+    // _firebaseMessaging.getToken().then((token) => print(token));
+  }
+  Future<List<Student>> getStudentList() async {
+    await Future.delayed(const Duration(seconds: 3));
+    // Load Json Data
+    final response = await http.get(Uri.parse(Env.URL_PREFIX));
+
+    final items = json.decode(response.body).cast<Map<String, dynamic>>();
+    List<Student> students = items.map<Student>((json) {
+      return Student.fromJson(json);
+    }).toList();
+
+    return students;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: studentListKey,
+      appBar: AppBar(
+        title: const Text('Students List'),
+      ),
+      body: Center(
+        child: FutureBuilder<List<Student>>(
+          future: students,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            // By default, show a loading spinner.
+            if (!snapshot.hasData) return const CircularProgressIndicator();
+            // Render student lists
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                var data = snapshot.data[index];
+                return Card(
+                  child: ListTile(
+                    leading: const Icon(Icons.person),
+                    trailing: const Icon(Icons.view_list),
+                    title: Text(
+                      data.name,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Details(student: data)),
+                      );
+                    },
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+         onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const Create()),
+          );
+         },
+      ),
+    );
+  }
+}
